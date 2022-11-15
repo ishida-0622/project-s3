e139731702bc1afb122e0f20694f1f17e643912a
Watabe <gauu14@gmail.com>
2022-11-15 12:28:20 +0900

    現在の日付時刻のフォーマットの変更 yyyy-MM-dd  hh:mm:ss

---
+++ src/modules/getNowDate.ts
@@ -0,0 +1,18 @@
+const getNowDate = () => {
+    const date = new Date();
+    const str =
+        date.getFullYear() +
+        "-" +
+        ("0" + (date.getMonth() + 1)).slice(-2) +
+        "-" +
+        ("0" + date.getDate()).slice(-2) +
+        " " +
+        ("0" + date.getHours()).slice(-2) +
+        ":" +
+        ("0" + date.getMinutes()).slice(-2) +
+        ":" +
+        ("0" + date.getSeconds()).slice(-2);
+    return str;
+};
+
+export default getNowDate;
