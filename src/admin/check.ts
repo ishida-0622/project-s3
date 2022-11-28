0a182c466a0945663a467a470b820422a672d3a1
kentaro ishida <ken.babuo@gmail.com>
2022-11-28 13:39:58 +0900

    update: 関数名の変更に伴う修正

--- src/admin/check.ts
+++ src/admin/check.ts
@@ -1,10 +1,10 @@
 import { db } from "../firebase/firebaseConfig";
 import { doc, getDoc } from "firebase/firestore";
 import { userConverter } from "../types/firestoreTypes";
-import getUserData from "../modules/getUserData";
+import getLoginUser from "../modules/getLoginUser";
 
 (async () => {
-    const user = await getUserData();
+    const user = await getLoginUser();
     if (!user) {
         location.href = "/";
         return;
