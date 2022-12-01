de90d7f718616992deb4384c10d6e3d6f352c279
kentaro ishida <ken.babuo@gmail.com>
2022-12-01 09:36:53 +0900

    update: 拡張子の変更

--- src/pages/congestion/index.ts
+++ src/pages/congestion/index.ts
@@ -4,7 +4,7 @@ import { storage } from "../../firebase/firebaseConfig";
 const main = () => {
     const shopName = new URL(location.href).searchParams.get("shop_name");
     const imageElement = document.querySelector("img");
-    getDownloadURL(ref(storage, `congestion/${shopName}.jpg`)).then((url) => {
+    getDownloadURL(ref(storage, `congestion/${shopName}.png`)).then((url) => {
         if (imageElement) {
             imageElement.src = url;
         }
