d13c4538da1c49f058ff970f7ed40eede403deb6
kentaro ishida <ken.babuo@gmail.com>
2022-12-06 18:17:13 +0900

    clean: コメントの追加

--- src/admin/check.ts
+++ src/admin/check.ts
@@ -3,14 +3,21 @@ import { doc, getDoc } from "firebase/firestore";
 import { userConverter } from "../types/firestoreTypes";
 import getLoginUser from "../modules/getLoginUser";
 
+// ログイン中のユーザーがadmin権限を持っているかをチェックする
 (async () => {
+    // ログイン中のユーザーを取得
     const user = await getLoginUser();
+
+    // ログイン中でなかったらTOPに飛ばす
     if (!user) {
         location.href = "/";
         return;
     }
+
+    // DBからユーザー情報を取得
     getDoc(doc(db, `users/${user.uid}`).withConverter(userConverter)).then(
         (d) => {
+            // moll_adminでなかったらTOPに飛ばす
             if (d.data()?.type !== "moll_admin") {
                 location.href = "/";
             }
