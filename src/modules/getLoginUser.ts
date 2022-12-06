5cb7fdfc999aa8766168b8ef40a66d9f592dc293
kentaro ishida <ken.babuo@gmail.com>
2022-12-06 17:48:58 +0900

    update

--- src/modules/getLoginUser.ts
+++ src/modules/getLoginUser.ts
@@ -1,8 +1,7 @@
-import { getAuth, onAuthStateChanged, User } from "firebase/auth";
-import { app } from "../firebase/firebaseConfig";
+import { onAuthStateChanged, User } from "firebase/auth";
+import { auth } from "../firebase/firebaseConfig";
 
 const getLoginUser = (): Promise<User | null> => {
-    const auth = getAuth(app);
     return new Promise((resolve) => {
         onAuthStateChanged(auth, (user) => {
             resolve(user);
