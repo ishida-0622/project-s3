41226d426e568e622fa309d37a65530b0df6a277
kentaro ishida <ken.babuo@gmail.com>
2023-01-11 10:32:55 +0900

    add: 検索バーの追加

--- src/pages/map/index.ts
+++ src/pages/map/index.ts
@@ -1,4 +1,66 @@
+import { db } from "../../firebase/firebaseConfig";
+import { collection, getDocs } from "firebase/firestore";
+import { storeInfo, storeInfoConverter } from "../../types/firestoreTypes";
 import FloorMap, { floor } from "./FloorMap";
+import sleep from "../../modules/sleep";
+
+const searchBar = document.getElementById(
+    "search-bar"
+) as HTMLInputElement | null;
+
+if (!searchBar) {
+    throw new Error("search bar element is not found");
+}
+
+const searchResultArea = document.getElementById(
+    "search-result"
+) as HTMLDivElement | null;
+
+if (!searchResultArea) {
+    throw new Error("search result area is not found");
+}
+let shopList: storeInfo[] = [];
+let showShopList: storeInfo[] = [];
+
+const shopSearch = (inputted: string) => {
+    const reg = new RegExp(inputted);
+    showShopList = shopList.filter((v) => v.store_name.match(reg));
+    searchResultArea.innerHTML = "";
+    showShopList.forEach((v) => {
+        const e = document.createElement("div");
+        const a = document.createElement("a");
+        a.href = `./?goal=${v.store_name}&goal_floor=${v.floor}`;
+        a.text = v.store_name;
+        e.appendChild(a);
+        searchResultArea.appendChild(e);
+    });
+};
+
+searchBar.onfocus = () => {
+    searchResultArea.style.display = "";
+};
+
+searchBar.onblur = async () => {
+    await sleep(100);
+    searchResultArea.style.display = "none";
+};
+
+(async () => {
+    shopList = (
+        await getDocs(
+            collection(db, "store_info").withConverter(storeInfoConverter)
+        )
+    ).docs.map((v) => v.data());
+    shopSearch("");
+})();
+
+searchBar.oninput = (e) => {
+    const { target } = e;
+    if (!(target instanceof HTMLInputElement)) {
+        throw new Error("search bar element is not input element");
+    }
+    shopSearch(target.value);
+};
 
 const main = async () => {
     const params = new URL(location.href).searchParams;
