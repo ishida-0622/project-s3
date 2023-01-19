3953d00fc29b7ed273343234e65eaed73100c9db
kentaro ishida <ken.babuo@gmail.com>
2023-01-19 10:45:05 +0900

    update: 更新日時と更新ボタンの追加

--- src/pages/congestion/index.ts
+++ src/pages/congestion/index.ts
@@ -1,14 +1,39 @@
-import { getDownloadURL, ref } from "firebase/storage";
+import { getDownloadURL, ref, getMetadata } from "firebase/storage";
+import utcToJst from "../../modules/utcToJst";
 import { storage } from "../../firebase/firebaseConfig";
 
-const main = () => {
-    const shopName = new URL(location.href).searchParams.get("shop_name");
-    const imageElement = document.querySelector("img");
-    getDownloadURL(ref(storage, `congestion/${shopName}.png`)).then((url) => {
+const shopName = new URL(location.href).searchParams.get("shop_name");
+const imageElement = document.querySelector("img");
+const timeStampElement = document.getElementById("timestamp");
+const reloadButton = document.getElementById("reload");
+
+if (!shopName) {
+    location.href = "/shop-list";
+}
+
+if (!reloadButton) {
+    throw new Error("reload button is not found");
+}
+
+if (!(imageElement && timeStampElement)) {
+    throw new Error("element is not found");
+}
+
+const reloadImage = () => {
+    const r = ref(storage, `congestion/${shopName}.png`);
+    getDownloadURL(r).then((url) => {
         if (imageElement) {
             imageElement.src = url;
         }
     });
+    getMetadata(r).then((metadata) => {
+        const [yyyymmdd, hhmmss] = metadata.timeCreated.split(/[A-Z]/);
+        timeStampElement.textContent = utcToJst(yyyymmdd, hhmmss.slice(0, 8));
+    });
 };
 
-main();
+reloadImage();
+
+reloadButton.addEventListener("click", () => {
+    reloadImage();
+});
