330a4daba1a431aecf3d67f906f425f1d2fcaacc
kentaro ishida <ken.babuo@gmail.com>
2023-01-19 10:44:11 +0900

    add: UTCをJSTに変換する関数

---
+++ src/modules/utcToJst.ts
@@ -0,0 +1,24 @@
+/**
+ * yyyy-MM-dd, hh:mm:ssの形式で渡されたUTCをJSTに変換する関数
+ * @param yyyyMMdd yyyy-MM-dd
+ * @param hhmmss hh:mm:ss
+ * @returns yyyy-MM-dd+(hh+9)/60 hh+9:mm:ss
+ */
+const utcToJst = (yyyyMMdd: string, hhmmss: string) => {
+    const yyyyMMddReg = /^[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])$/;
+    const hhmmssReg = /^([01][0-9]|2[0-3]):[0-5][0-9]:[0-5][0-9]$/;
+    if (!yyyyMMddReg.test(yyyyMMdd)) {
+        throw new Error("yyyyMMdd not match regexp from UTC to JST");
+    }
+    if (!hhmmssReg.test(hhmmss)) {
+        throw new Error("hhmmss not match regexp from UTC to JST");
+    }
+
+    const [yyyy, MM, dd] = yyyyMMdd.split("-").map((v) => Number(v));
+    const [hh, mm, ss] = hhmmss.split(":").map((v) => Number(v));
+    return `${yyyy}-${MM}-${dd + Math.floor((hh + 9) / 60)} ${
+        (hh + 9) % 24
+    }:${mm}:${ss}`;
+};
+
+export default utcToJst;
